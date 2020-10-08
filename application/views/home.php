53ef33599083019157ae8c064fefef246662a933
yogesh avhale <you@example.com>
2020-10-08 23:32:38 +0530

    New page aedded in view..

---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>User Registration Form</title>
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> 
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h4>Welcome Dear...</h4>
+            </div>
+        </div>
+    </div>
+</body>
+
+   
