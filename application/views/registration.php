1b326eb7be49d75bf497c48b47deaec0e1b37130
yogesh-avhale <46989330+yogesh-avhale@users.noreply.github.com>
2020-10-08 23:27:22 +0530

    Update registration.php

--- application/views/registration.php
+++ application/views/registration.php
@@ -39,34 +39,33 @@
         <div class="col-md-6">
             <form action="" method="post" id="fileForm" role="form">
                 <fieldset><legend class="text-center">Valid information is required to register. <span class="req"><small> required *</small></span></legend>
-
                 <div class="form-group">
                 <label for="phonenumber"><span class="req">* </span> Phone Number: </label>
-                        <input required type="text" name="phonenumber" id="phone" class="form-control phone" maxlength="28" onkeyup="validatephone(this);" placeholder="not used for marketing"/> 
+										<input required type="text" name="phonenumber" id="phone" class="form-control phone" maxlength="28" onkeyup="validatephone(this);" placeholder="not used for marketing"/> 
                 </div>
 
                 <div class="form-group"> 	 
                     <label for="firstname"><span class="req">* </span> First name: </label>
-                        <input class="form-control" type="text" name="firstname" id = "txt" onkeyup = "Validate(this)" required /> 
-                            <div id="errFirst"></div>    
+										<input class="form-control" type="text" name="firstname" id = "txt" onkeyup = "Validate(this)" required /> 
+										<div id="errFirst"></div>    
                 </div>
 
                 <div class="form-group">
                     <label for="lastname"><span class="req">* </span> Last name: </label> 
                         <input class="form-control" type="text" name="lastname" id = "txt" onkeyup = "Validate(this)" placeholder="hyphen or single quote OK" required />  
-                            <div id="errLast"></div>
+												<div id="errLast"></div>
                 </div>
 
                 <div class="form-group">
                     <label for="email"><span class="req">* </span> Email Address: </label> 
                         <input class="form-control" required type="text" name="email" id = "email"  onchange="email_validate(this.value);" />   
-                            <div class="status" id="status"></div>
+												<div class="status" id="status"></div>
                 </div>
 
                 <div class="form-group">
                     <label for="username"><span class="req">* </span> User name:  <small>This will be your login user name</small> </label> 
                         <input class="form-control" type="text" name="username" id = "txt" onkeyup = "Validate(this)" placeholder="minimum 6 letters" required />  
-                            <div id="errLast"></div>
+												<div id="errLast"></div>
                 </div>
 
                 <div class="form-group">
@@ -75,16 +74,14 @@
 
                     <label for="password"><span class="req">* </span> Password Confirm: </label>
                         <input required name="password" type="password" class="form-control inputpass" minlength="4" maxlength="16" placeholder="Enter again to validate"  id="pass2" onkeyup="checkPass(); return false;" />
-                            <span id="confirmMessage" class="confirmMessage"></span>
+												<span id="confirmMessage" class="confirmMessage"></span>
                 </div>
 
                 <div class="form-group">
-
                     <?php //$date_entered = date('m/d/Y H:i:s'); ?>
                     <input type="hidden" value="<?php //echo $date_entered; ?>" name="dateregistered">
                     <input type="hidden" value="0" name="activate" />
                     <hr>
-
                     <input type="checkbox" required name="terms" onchange="this.setCustomValidity(validity.valueMissing ? 'Please indicate that you accept the Terms and Conditions' : '');" id="field_terms">   <label for="terms">I agree with the <a href="terms.php" title="You may read our terms and conditions by clicking on this link">terms and conditions</a> for Registration.</label><span class="req">* </span>
                 </div>
 
@@ -110,7 +107,7 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script>
-    function checkPass()
+function checkPass()
 {
     //Store the password field objects into variables ...
     var pass1 = document.getElementById('pass1');
